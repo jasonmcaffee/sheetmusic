c625392333f0abf66de6b8b1701f16b6c0d83176
jason mcaffee <jasonlmcaffee@gmail.com>
2019-06-02 16:45:07 -0600

    render of staff working.

--- src/components/SheetMusic.jsx
+++ src/components/SheetMusic.jsx
@@ -21,18 +21,57 @@ function drawCanvas({canvas, height, width}){
   canvas.setAttribute('width', width);
   canvas.setAttribute('height', height);
 
-  const context = canvas.getContext('2d');
-  // Reset the current path
-  context.beginPath();
-  // Staring point (10,45)
-  context.moveTo(width, height);
-  // End point (180,47)
-  context.lineTo(0, 0);
+  const ctx = canvas.getContext('2d');
+  styleLine({ctx, lineWidth: 1});
+  // drawLine({ctx, startX: 0, startY: 0, endX: width, endY: height});
+  // drawHorizontalLine({ctx, startX: 0, startY: 20, width, });
 
-  context.moveTo(width, 0);
-  context.lineTo(0, 0);
-  // Make the line visible
-  context.lineWidth = 1;
-  context.stroke();
+  const padding = 10;
+  const startXPadded = 0 + padding;
+  const widthPadded = width - (2 * padding);
+  drawStaff({ctx, staffLineDistance: 15, startX: startXPadded, startY: 10, width: widthPadded, });
 
+}
+
+function drawStaff({ctx, startX = 0, startY = 0, width = 100, staffLineDistance = 10, }){
+  const numberOfStaffLines = 5;
+  let lineStartY = startY;
+  for(let i = 0; i < numberOfStaffLines; ++i){
+    drawHorizontalLine({ctx, startY: lineStartY, width, startX});
+    lineStartY += staffLineDistance;
+  }
+
+  const staffHeight = (numberOfStaffLines - 1) * staffLineDistance;
+  const numberOfBars = 6;
+  const barLineDistance = width / (numberOfBars - 1);
+  drawStaffBars({ctx, height: staffHeight, startY, startX, barLineDistance, numberOfBars, });
+}
+
+function drawStaffBars({ctx, numberOfBars = 6, startX = 0, startY = 0, height = 100, barLineDistance = 10}){
+  let lineStartX = startX;
+  for(let i = 0; i < numberOfBars; ++i){
+    drawVerticalLine({ctx, startX: lineStartX, startY, height, });
+    lineStartX += barLineDistance;
+  }
+}
+
+function drawHorizontalLine({ ctx, startX, startY, width, }){
+  const endX = startX + width;
+  const endY = startY;
+  drawLine({ ctx, startX, startY, endX, endY, });
+}
+function drawVerticalLine({ ctx, startX, startY, height, }){
+  const endX = startX;
+  const endY = startY + height;
+  drawLine({ ctx, startX, startY, endX, endY, });
+}
+
+function drawLine({ ctx, startX, startY, endX, endY, }){
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+}
+
+function styleLine({ ctx, lineWidth = 1, }){
+  ctx.lineWidth = lineWidth;
 }
